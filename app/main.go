7c97b8ea14f398db724603bcd2b0f92010e783b0
cultab <rroarck@gmail.com>
2023-03-03 21:31:38 +0200

    feat(form): login form scaffold code

--- app/main.go
+++ app/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"errors"
-	"fmt"
 	"log"
+	"net/http"
+	"net/http/cookiejar"
 	"os"
 	"path/filepath"
 
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/Huray-hub/eclass-utils/assignments/config"
+	auth "github.com/Huray-hub/eclass-utils/authentication"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func init() {
@@ -33,7 +36,7 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    // defer func() { NOTE: uhhhhhhhh, remove? ask
+	// defer func() { NOTE: uhhhhhhhh, remove? ask
 	// 	if err = file.Close(); err != nil {
 	// 		log.Fatal(err.Error())
 	// 	}
@@ -44,25 +47,58 @@ func init() {
 }
 
 func updateConfiguration(o config.Options) {
-    log.Print("Not Implemented: updating the configuration file.")
+	log.Print("Not Implemented: updating the configuration file.")
 }
 
 func main() {
-    opts, creds, err := config.Import()
+	opts, creds, err := config.Import()
 	if err != nil {
 		log.Fatal(err)
 	}
-    config := config.Config{
-        Credentials: *creds,
-        Options: *opts,
+	conf := config.Config{
+		Credentials: *creds,
+		Options:     *opts,
+	}
+
+	{
+		auth_creds := auth.Credentials{
+			Username: "asdf",
+			Password: "asdf",
+		}
+
+		jar, err := cookiejar.New(nil)
+		if err != nil {
+			return
+		}
+
+		client := &http.Client{
+			Jar: jar,
+		}
+		err = auth.Login(context.Background(), "https://"+conf.Options.BaseDomain, auth_creds, client)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+    logged_in := false // TODO: find out how to check if we logged in correctly
+    var ret tea.Model
+
+    if ! logged_in {
+        p := tea.NewProgram(NewForm())
+        if ret, err = p.Run(); err != nil {
+            log.Fatalf("Alas, there's been an error: %v", err)
+        }
+
+        login_form := ret.(form) // NOTE: should always succeed
+        opts.BaseDomain = login_form.fields[0].Value()
+        conf.Credentials.Username = login_form.fields[1].Value()
+        conf.Credentials.Password = login_form.fields[2].Value()
     }
 
-    // config.Options.ExcludedCourses[""] = struct{}{}
-    m := NewList(config)
+
+	m := NewList(conf)
 	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+		log.Fatalf("Alas, there's been an error: %v", err)
 	}
 
 }
